74de0323c45a903c1552cea12d34fcb20961b7c9
Daler-web-dev <77038375+Daler-web-dev@users.noreply.github.com>
2022-01-06 23:14:08 +0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import {products} from "./data.js"
+import {products, questions} from "./data.js"
 
 let modalWindow = document.querySelector('.side-bar')
 let modal_bg = document.querySelector('.modal-bg')
@@ -31,8 +31,7 @@ for(let item of products) {
     span.innerHTML = item.title
     p.innerHTML = item.description
     for(let key in item.about) {
-        console.log(key);
-        span2.innerHTML += `<b> ${key}:</b>  ${item.about[key]}  <br>`
+        span2.innerHTML += `<b> ${key}:</b>  ${item.about[key]}  <br> <br>`
     }
     
 
@@ -47,28 +46,34 @@ for(let item of products) {
     products_place.append(div)
 }
 
-{/* <div class="item">
-    <img src="img/product/sncrs.png" alt="" />
-    <div class="description">
-        <span class="h3">{{ item.name }}</span>
-        <p class="note">{{ item.about }}</p>
-        <div
-            v-for="(desc, key) in item.description"
-            :key="desc.key"
-        >
-            <span>{{key}}: <b>{{ desc }}</b></span>
-        </div>
-
-        <div class="active_button">
-            Добавить в корзину +
-        </div>
-
-        <div class="cart-to">
-            <img src="img/greyCart.png" alt="" />
-            <span>В корзину</span>
-        </div>
-    </div>
-</div> */}
-
 
 console.log(products);
+
+let whyWe = document.querySelector('.why-we')
+console.log(whyWe);
+
+for(let item of questions) {
+    let div = document.createElement('div')
+    let image = document.createElement('img')
+    let description = document.createElement('div')
+    let span = document.createElement('span')
+    let p = document.createElement('p')
+    let span2 = document.createElement('span')
+    
+    span.innerHTML = item.title
+    p.innerHTML = item.description
+    for(let key in item.about) {
+        span2.innerHTML += `<b> ${key}:</b>  ${item.about[key]}  <br> <br>`
+    }
+    
+
+    image.setAttribute('src', item.img)
+    div.classList.add('item', 'whyWeItem')
+    description.classList.add('description')
+    span.classList.add('h3')
+    p.classList.add('note')
+
+    description.append(span, p, span2)
+    div.append(image, description)
+    whyWe.append(div)
+}
